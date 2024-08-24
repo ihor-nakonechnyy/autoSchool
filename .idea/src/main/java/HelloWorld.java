b6070a8aef6ae21199ef6361d7e220033f5a068b
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2024-08-24 20:55:26 +0200

    removed comments

--- .idea/src/main/java/HelloWorld.java
+++ .idea/src/main/java/HelloWorld.java
@@ -7,17 +7,10 @@ public class HelloWorld {
     }
 
     public static void printHelloName() {
-        // Создание объекта Scanner для чтения данных с консоли
         Scanner scanner = new Scanner(System.in);
-
-        // Запрос имени у пользователя
         System.out.print("Enter your name: ");
         String name = scanner.nextLine();
-
-        // Вывод приветствия с введенным именем
         System.out.println("Hello, " + name + "!");
-
-        // Закрытие сканера
         scanner.close();
     }
 }
