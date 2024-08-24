befbea6d3b09c32d71e16cde1b04903f1d7a92b1
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2024-08-24 20:46:18 +0200

    added the hello world class

---
+++ .idea/src/main/java/HelloWorld.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+        printHelloName();
+    }
+
+    public static void printHelloName() {
+        // Создание объекта Scanner для чтения данных с консоли
+        Scanner scanner = new Scanner(System.in);
+
+        // Запрос имени у пользователя
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        // Вывод приветствия с введенным именем
+        System.out.println("Hello, " + name + "!");
+
+        // Закрытие сканера
+        scanner.close();
+    }
+}
