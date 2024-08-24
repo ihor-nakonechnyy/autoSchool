cb6375326419df4f6f31d770296a7227a357fa84
ihor-nakonechnyy <nakonechnyy.i@gmail.com>
2024-08-24 20:52:23 +0200

    commented default message

--- .idea/src/main/java/HelloWorld.java
+++ .idea/src/main/java/HelloWorld.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        //System.out.println("Hello, world!");
         printHelloName();
     }
 
